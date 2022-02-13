029735f098e92158a17371720339b298605714a5
LukasHaken <62891361+HakenTrixi@users.noreply.github.com>
2022-02-13 12:32:39 +0100

    Add card move by pull request action type
    * check duplicate attachment
    * show error in state
    * add card move possibility by PR

--- dist/index.js
+++ dist/index.js
@@ -11248,18 +11248,49 @@ __nccwpck_require__.r(__webpack_exports__);
 const { context = {} } = _actions_github__WEBPACK_IMPORTED_MODULE_2__;
 const { pull_request, head_commit } = context.payload;
 
-const CARD_ID_PATTERN = /^https:\/\/trello.com\/c\/(\w{8})/g;
+const CARD_ID_PATTERN = /^https:\/\/trello.com\/c\/([a-zA-Z0-9]{8})/g;
 const TRELLO_API_KEY = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('trello-api-key', { required: true });
 const TRELLO_AUTH_TOKEN = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('trello-auth-token', { required: true });
+const TRELLO_BOARD_REOPEN_LIST_ID = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('trello-board-reopen-list-id', { required: false });
+const TRELLO_BOARD_NEEDS_CODE_REVIEW_LIST_ID = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('trello-board-needs-code-review-list-id', { required: false });
+const TRELLO_BOARD_NEEDS_ACCEPTATION_LIST_ID = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('trello-board-needs-acceptation-list-id', { required: false });
 
 function getCardID(message) {
   console.log(`getCardID(${message})`);
-  let cardId = CARD_ID_PATTERN.exec(message)[1];
-  return cardId ? cardId : null ;
+  if (CARD_ID_PATTERN.test(message)) {
+      let cardId = CARD_ID_PATTERN.exec(message)[1];
+      console.log(`Unique card's ID is ${cardId}`);
+      return cardId;  
+  }
+  else {
+    throw new Error(`Message ${message} doesn't begin on card's short link`);
+  }
+  
+}
+
+async function isLinkAttachedToCard(card, link) {
+  console.log(`isPullRequestAttachedToCard(${card}, ${link})`);
+  let url = `https://api.trello.com/1/cards/${card}/attachments`;
+
+  return await axios__WEBPACK_IMPORTED_MODULE_0__.get(url, {
+    params: {
+      key: trelloApiKey,
+      token: trelloAuthToken
+    }
+  }).then(response => {
+    return response.data.findIndex(v => v.url === link) != -1;
+  }).catch(error => {
+    console.error(url, `Error ${error.response.status} ${error.response.statusText}`);
+    return false;
+  });
 }
 
 async function addAttachmentToCard(card, link) {
   console.log(`addAttachmentToCard(${card}, ${link})`);
+  if (isLinkAttachedToCard(card, link)) {
+    // Don't want to duplicate link in card
+    return null;
+  }
   let url = `https://api.trello.com/1/cards/${card}/attachments`;
   return await axios__WEBPACK_IMPORTED_MODULE_0__.post(url, {
     key: TRELLO_API_KEY,
@@ -11267,29 +11298,19 @@ async function addAttachmentToCard(card, link) {
     url: link
   }).then(response => {
     return response.status == 200;
-  }).catch(error => {
-    console.error(url, `Error ${error.response.status} ${error.response.statusText}`);
-    return null;
   });
 }
 
-async function moveCardToList(board, card, list) {
-  console.log(`moveCardToList(${board}, ${card}, ${list})`);
-  let listId = await getListOnBoard(board, list);
-  if (listId && listId.length > 0) {
+async function moveCardToList(board, card, listId) {
+  console.log(`moveCardToList(${board}, ${card}, ${listId})`);
     let url = `https://api.trello.com/1/cards/${card}`;
-    return await axios.put(url, {
+    return await axios__WEBPACK_IMPORTED_MODULE_0__.put(url, {
       key: TRELLO_API_KEY,
       token: TRELLO_AUTH_TOKEN, 
       idList: listId
     }).then(response => {
       return response && response.status == 200;
-    }).catch(error => {
-      console.error(url, `Error ${error.response.status} ${error.response.statusText}`);
-      return null;
     });
-  }       
-  return null;
 }
 
 async function handleHeadCommit(data) {
@@ -11298,29 +11319,42 @@ async function handleHeadCommit(data) {
   let message = data.message;
   let user = data.author.name;
   let card = await getCardID(message);
-  if (card && card.length > 0) {
-      await addAttachmentToCard(card, url);
-  }
+  await addAttachmentToCard(card, url);
 }
 
-async function handlePullRequest(data) {
-  console.log("handlePullRequest", data);
+async function handlePullRequest(data, actionType) {
+  console.log("handlePullRequest", data, actionType);
   let url = data.html_url || data.url;
   let message = data.title;
   let user = data.user.name;
   let card = await getCardID(message);
-  if (card && card.length > 0) {
-      await addAttachmentToCard(card, url);
+  await addAttachmentToCard(card, url);
+  if (TRELLO_BOARD_REOPEN_LIST_ID && actionType == "reopened" ) {
+      moveCardToList(card, TRELLO_BOARD_REOPEN_LIST_ID);
+  }
+  else if (TRELLO_BOARD_NEEDS_CODE_REVIEW_LIST_ID && actionType == "opened" ) {
+      moveCardToList(card, TRELLO_BOARD_NEEDS_CODE_REVIEW_LIST_ID); 
+  }
+  else if (TRELLO_BOARD_NEEDS_ACCEPTATION_LIST_ID && actionType == "closed" ) {
+      moveCardToList(card, TRELLO_BOARD_NEEDS_ACCEPTATION_LIST_ID);
   }
 }
 
 async function run() {
-  if (head_commit && head_commit.message) {
-    handleHeadCommit(head_commit)
+
+  try {
+    console.log("Run context", context);
+    if (head_commit && head_commit.message) {
+      handleHeadCommit(head_commit)
+    }
+    else if (pull_request && pull_request.title) {
+      handlePullRequest(pull_request, context.action)
+    }
   }
-  else if (pull_request && pull_request.title) {
-    handlePullRequest(pull_request)
+  catch (error) {
+    _actions_core__WEBPACK_IMPORTED_MODULE_1__.setFailed(error.message);
   }
+  
 };
 
 run()
